6125e662dfadb9310ff267fc09a5197212d4224e
Donovan Dicks <donovan.dicks@outlook.com>
2022-07-08 00:56:14 -0400

    Add a custom GET Handler for the about route

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,5 +1,16 @@
 /** @jsx h */
 import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    // Render the page first
+    const resp = await ctx.render();
+    // Add a custom header to the response before returning it
+    resp.headers.set("X-Custom-Header", "Hello");
+    return resp;
+  },
+};
 
 export default function AboutPage() {
   return (
